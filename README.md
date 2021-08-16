598231d544bac4a6e978ec047b119b52e8d37b67
kanyu776 <185296836@qq.com>
2021-08-16 16:11:22 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-l32478
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-l32478)
---
+++ index.js
@@ -0,0 +1,3 @@
+// run `node index.js` in the terminal11
+
+console.log(`H1ell1o Node.js v${process.versions.node}!`);
